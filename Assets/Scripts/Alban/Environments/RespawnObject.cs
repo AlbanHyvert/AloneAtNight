a01e6beab9cf00684a6abf4c558d946be42d40d7
AlbanHyvert <40693949+AlbanHyvert@users.noreply.github.com>
2020-10-06 16:09:34 +0200

    [Respawn Object] Object with the script "RespawnObject" will now respawn at their starting position

---
+++ Assets/Scripts/Alban/Environments/RespawnObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RespawnObject : MonoBehaviour
+{
+    private Vector3 _lastValidPosition = Vector3.zero;
+
+    public Vector3 GetLastValidPosition { get { return _lastValidPosition; } }
+    public Vector3 SetLastValidPosition { set { _lastValidPosition = value; } }
+
+    private void Start()
+    {
+        if(_lastValidPosition == Vector3.zero)
+        {
+            _lastValidPosition = this.transform.position;
+        }
+    }
+}
