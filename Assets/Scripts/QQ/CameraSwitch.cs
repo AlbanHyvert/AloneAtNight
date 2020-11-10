c0458d99031d11d7be12dbcd680b35bf7536a1d8
Queadpool <q.lavet@gmail.com>
2020-11-10 14:54:33 +0100

    Modif switch cam

--- Assets/Scripts/QQ/CameraSwitch.cs
+++ Assets/Scripts/QQ/CameraSwitch.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class CameraSwitch : MonoBehaviour
 {
 
-    public GameObject cameraOne;
-    public GameObject cameraTwo;
+    public GameObject camera0, camera1, camera2, camera3, camera4, camera5, camera6, camera7, camera8;
 
-    AudioListener cameraOneAudioLis;
-    AudioListener cameraTwoAudioLis;
+    AudioListener camera0AudioLis, camera1AudioLis, camera2AudioLis, camera3AudioLis, camera4AudioLis, camera5AudioLis, camera6AudioLis, camera7AudioLis, camera8AudioLis;
 
     // Use this for initialization
     void Start()
     {
 
         //Get Camera Listeners
-        cameraOneAudioLis = cameraOne.GetComponent<AudioListener>();
-        cameraTwoAudioLis = cameraTwo.GetComponent<AudioListener>();
+        camera0AudioLis = camera0.GetComponent<AudioListener>();
+        camera1AudioLis = camera1.GetComponent<AudioListener>();
+        camera2AudioLis = camera2.GetComponent<AudioListener>();
+        camera3AudioLis = camera3.GetComponent<AudioListener>();
+        camera4AudioLis = camera4.GetComponent<AudioListener>();
+        camera5AudioLis = camera5.GetComponent<AudioListener>();
+        camera6AudioLis = camera6.GetComponent<AudioListener>();
+        camera7AudioLis = camera7.GetComponent<AudioListener>();
+        camera8AudioLis = camera8.GetComponent<AudioListener>();
 
         //Camera Position Set
         cameraPositionChange(PlayerPrefs.GetInt("CameraPosition"));
@@ -33,33 +38,50 @@ public class CameraSwitch : MonoBehaviour
     //UI JoyStick Method
     public void cameraPositonM()
     {
-        cameraChangeCounter();
+        cameraIncCounter();
     }
 
     //Change Camera Keyboard
     void switchCamera()
     {
-        if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt))
+        if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.LeftAlt))
         {
-            cameraChangeCounter();
+            cameraIncCounter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.V) || Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            cameraDecCounter();
         }
     }
 
     //Camera Counter
-    void cameraChangeCounter()
+    void cameraIncCounter()
     {
         int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
         cameraPositionCounter++;
         cameraPositionChange(cameraPositionCounter);
     }
 
+    void cameraDecCounter()
+    {
+        int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
+        cameraPositionCounter--;
+        cameraPositionChange(cameraPositionCounter);
+    }
+
     //Camera change Logic
     void cameraPositionChange(int camPosition)
     {
-        if (camPosition > 1)
+        if (camPosition > 8)
         {
             camPosition = 0;
         }
+        else if (camPosition < 0)
+        {
+            camPosition = 8;
+        }
+
 
         //Set camera position database
         PlayerPrefs.SetInt("CameraPosition", camPosition);
@@ -67,22 +89,111 @@ public class CameraSwitch : MonoBehaviour
         //Set camera position 1
         if (camPosition == 0)
         {
-            cameraOne.SetActive(true);
-            cameraOneAudioLis.enabled = true;
+            camera0.SetActive(true);
+            camera0AudioLis.enabled = true;
 
-            cameraTwoAudioLis.enabled = false;
-            cameraTwo.SetActive(false);
+            camera8AudioLis.enabled = false;
+            camera8.SetActive(false);
+
+            camera1AudioLis.enabled = false;
+            camera1.SetActive(false);
         }
 
         //Set camera position 2
         if (camPosition == 1)
         {
-            cameraTwo.SetActive(true);
-            cameraTwoAudioLis.enabled = true;
+            camera1.SetActive(true);
+            camera1AudioLis.enabled = true;
 
-            cameraOneAudioLis.enabled = false;
-            cameraOne.SetActive(false);
+            camera0AudioLis.enabled = false;
+            camera0.SetActive(false);
+
+            camera2AudioLis.enabled = false;
+            camera2.SetActive(false);
         }
 
+        if (camPosition == 2)
+        {
+            camera2.SetActive(true);
+            camera2AudioLis.enabled = true;
+
+            camera1AudioLis.enabled = false;
+            camera1.SetActive(false);
+
+            camera3AudioLis.enabled = false;
+            camera3.SetActive(false);
+        }
+
+        if (camPosition == 3)
+        {
+            camera3.SetActive(true);
+            camera3AudioLis.enabled = true;
+
+            camera2AudioLis.enabled = false;
+            camera2.SetActive(false);
+
+            camera4AudioLis.enabled = false;
+            camera4.SetActive(false);
+        }
+
+        if (camPosition == 4)
+        {
+            camera4.SetActive(true);
+            camera4AudioLis.enabled = true;
+
+            camera3AudioLis.enabled = false;
+            camera3.SetActive(false);
+
+            camera5AudioLis.enabled = false;
+            camera5.SetActive(false);
+        }
+
+        if (camPosition == 5)
+        {
+            camera5.SetActive(true);
+            camera5AudioLis.enabled = true;
+
+            camera4AudioLis.enabled = false;
+            camera4.SetActive(false);
+
+            camera6AudioLis.enabled = false;
+            camera6.SetActive(false);
+        }
+
+        if (camPosition == 6)
+        {
+            camera6.SetActive(true);
+            camera6AudioLis.enabled = true;
+
+            camera5AudioLis.enabled = false;
+            camera5.SetActive(false);
+
+            camera7AudioLis.enabled = false;
+            camera7.SetActive(false);
+        }
+
+        if (camPosition == 7)
+        {
+            camera7.SetActive(true);
+            camera7AudioLis.enabled = true;
+
+            camera6AudioLis.enabled = false;
+            camera6.SetActive(false);
+
+            camera8AudioLis.enabled = false;
+            camera8.SetActive(false);
+        }
+
+        if (camPosition == 8)
+        {
+            camera8.SetActive(true);
+            camera8AudioLis.enabled = true;
+
+            camera7AudioLis.enabled = false;
+            camera7.SetActive(false);
+
+            camera0AudioLis.enabled = false;
+            camera0.SetActive(false);
+        }
     }
 }
